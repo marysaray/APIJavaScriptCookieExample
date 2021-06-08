01d2c4254d8296774e5e478b5e9221e77009dd7a
Mary Saray <msaray.ms@outlook.com>
2021-06-08 16:10:44 -0700

    Added js-cookie script and ts/js to html file.

---
+++ ts/main.ts
@@ -0,0 +1,29 @@
+// window onload to wire all three buttons: 
+window.onload = function() {
+    let readCookie = document.getElementById("read-cookie");
+    let deleteCookie = document.getElementById("delete-cookie");
+    let createCookie = document.getElementById("create-cookie");
+
+    // set up function for each one for event onclick 
+    readCookie.onclick = readCookieData;
+    deleteCookie.onclick = deleteCookieData;
+    createCookie.onclick = createCookieData;
+}
+/**
+ * Read Cookies
+ */
+function readCookieData(){
+
+}
+/**
+ * Delete Cookies
+ */
+function deleteCookieData(){
+
+}
+/**
+ * Create Cookies
+ */
+function createCookieData(){
+
+}
