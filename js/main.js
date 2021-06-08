8c0086d51ff25b571746eaf61b45bc82c4ffd2b9
Mary Saray <msaray.ms@outlook.com>
2021-06-08 16:55:36 -0700

    Established cookie concept with an object item.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+var Item = (function () {
+    function Item() {
+    }
+    return Item;
+}());
 window.onload = function () {
     var readCookie = document.getElementById("read-cookie");
     var deleteCookie = document.getElementById("delete-cookie");
@@ -6,9 +11,25 @@ window.onload = function () {
     deleteCookie.onclick = deleteCookieData;
     createCookie.onclick = createCookieData;
 };
+var thisCookie = "TestCookie";
 function readCookieData() {
+    var data = JSON.parse(Cookies.get(thisCookie));
+    if (data != undefined) {
+        console.log(data);
+    }
+    else {
+        console.log("No cookie was found");
+    }
 }
 function deleteCookieData() {
+    Cookies.remove(thisCookie);
+    console.log("Cookie was removed.");
 }
 function createCookieData() {
+    var item = new Item();
+    item.title = " This is an item";
+    item.isComplete = false;
+    var TwoWeeksAsDays = 14;
+    Cookies.set(thisCookie, JSON.stringify(item), { expires: TwoWeeksAsDays });
+    console.log("Cookie was created.");
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,8 @@
+class Item{
+    title:string;
+    isComplete:boolean;
+}
+
 // window onload to wire all three buttons: 
 window.onload = function() {
     let readCookie = document.getElementById("read-cookie");
@@ -9,21 +14,39 @@ window.onload = function() {
     deleteCookie.onclick = deleteCookieData;
     createCookie.onclick = createCookieData;
 }
+/* create const to reduce redundancy: intellisens */
+const thisCookie = "TestCookie";
 /**
  * Read Cookies
  */
 function readCookieData(){
-
+    let data:Item = JSON.parse(Cookies.get(thisCookie));
+    if(data != undefined){
+        console.log(data);
+    } else {
+        console.log("No cookie was found");
+    }
 }
 /**
  * Delete Cookies
  */
 function deleteCookieData(){
-
+    Cookies.remove(thisCookie);
+    console.log("Cookie was removed.");
 }
 /**
  * Create Cookies
  */
 function createCookieData(){
+    let item = new Item();
+    item.title = " This is an item";
+    item.isComplete = false;
 
+    const TwoWeeksAsDays = 14;
+
+    Cookies.set(thisCookie,
+                JSON.stringify(item),
+                {expires: TwoWeeksAsDays});
+
+    console.log("Cookie was created.");
 }
